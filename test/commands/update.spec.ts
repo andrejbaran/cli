ff724e82dfbef63ea52de291c80c3b9afde2a342
Nikos Milonas <nikos+remax@hackcapital.com>
2019-10-30 21:06:29 +0000

    Create tests for Ops Update command

---
+++ test/commands/update.spec.ts
@@ -0,0 +1,63 @@
+import * as Config from '@oclif/config'
+import Update from '~/commands/update'
+import { Services } from '~/types'
+import { WORKFLOW } from '~/constants/opConfig'
+import { AnalyticsService } from '~/services/Analytics'
+import { ux } from '@cto.ai/sdk'
+
+import { sleep } from '../utils'
+import {
+  createMockTeam,
+  createMockConfig,
+  createMockTokens,
+  createMockUser,
+} from '../mocks'
+
+let cmd: Update
+
+let config: Config.IConfig
+
+beforeEach(async () => {
+  config = await Config.load()
+})
+
+afterEach(async () => {
+  await sleep(500)
+})
+
+describe('testSendAnalytics', () => {
+  test('Test if analytics are sent correctly on run', async () => {
+    const mockAnalyticsService = new AnalyticsService()
+    mockAnalyticsService.track = jest.fn().mockReturnValue(null)
+
+    const selectedTemplate = WORKFLOW
+
+    ux.prompt = jest.fn().mockReturnValue({ template: selectedTemplate })
+
+    cmd = new Update([], config, {
+      analytics: mockAnalyticsService,
+    } as Services)
+
+    cmd.readConfig = jest.fn().mockReturnValue({
+      tokens: createMockTokens({}),
+    })
+    const mockTeam = createMockTeam({})
+    const mockUser = createMockUser({
+      id: 'FAKE_ID',
+      username: 'FAKE_NAME',
+      email: 'fake@email.com',
+    })
+    const mockConfig = createMockConfig({ user: mockUser, team: mockTeam })
+
+    cmd.askQuestion = jest.fn().mockReturnValue(true)
+    cmd.updateVersion = jest.fn().mockReturnValue(false)
+    cmd.log = jest.fn().mockReturnValue(null)
+    cmd.isLoggedIn = jest.fn().mockReturnValue(mockConfig)
+    cmd.user = mockUser
+    cmd.team = mockTeam
+
+    await cmd.run()
+
+    expect(mockAnalyticsService.track.mock.calls.length).toBe(1)
+  })
+})
