832dd37279cfb1714701e1ddab97fff5fe35d58a
steve hiehn <stevehiehn@hackcapital.com>
2019-11-06 13:44:11 -0800

    Register sending api request

--- src/commands/secrets/register.ts
+++ src/commands/secrets/register.ts
@@ -1,9 +1,10 @@
 import { ux } from '@cto.ai/sdk'
 import Command, { flags } from '~/base'
-import { Config, Team } from '~/types'
-import { asyncPipe } from '~/utils'
+import {Config, Team, User} from '~/types'
+import {asyncPipe, validCharsTeamName} from '~/utils'
 import {ConfigError, APIError, InvalidTeamNameFormat} from '~/errors/CustomErrors'
 import {CreateInputs} from "~/commands/team/create";
+import {RemoveInputs} from "~/commands/remove";
 
 const { white, reset } = ux.colors
 interface displayTeam extends Team {
@@ -30,51 +31,42 @@ export default class SecretsRegister extends Command {
     }
   }
 
-  checkHealth = async (inputs: CreateInputs): Promise<CreateInputs> => {
+  validateRegisterInput = async (input: string): Promise<boolean | string> => {
     try {
-      const { name } = inputs
-      const res: { data: Team } = await this.services.api.find(
-        'health',
-        { name },
-        // { headers: { Authorization: this.accessToken } },
-      )
-
-      console.log("########################################", res)
-
-      const team: Team = { id: res.data.id, name: res.data.name }
-      return { ...inputs, team }
+      if (!input) {
+        return `😞 Sorry, the value cannot be empty`
+      }
+      return true
     } catch (err) {
-      this.debug('%O', err)
       throw new InvalidTeamNameFormat(err)
     }
   }
-
+//🔗
   promptForSecretsProviderCredentials = async (
     inputs: RegisterInputs,
   ): Promise<RegisterInputs> => {
     const { url } = await ux.prompt<{ url: string }>({
       type: 'input',
       name: 'url',
-      message: `\nProvide a URL for your secrets provider ${reset.green(
+      message: `\n🔐 Register your secret storage to share secrets and passwords with team ${reset.blueBright(`${inputs.activeTeam.name}`)}    \n${reset.grey('Enter your secret storage')} ${reset.blue('url')} ${reset.grey('and')} ${reset.blue('access token')} ${reset.grey('. Change the team to with which \nthe secret storage will be registered by running')} ${reset.blue('ops team:switch')}${reset.grey('.')} \n${white('Link your secret storage to your team')} ${reset.green(
         '→',
-      )}  \n🏀 ${white('URL')} `,
+      )}`,
       afterMessage: `${reset.green('✓')} URL    `,
-      // TODO: STEVE YOU NEED TO VALIDATE STUFF
-      // validate: this.validateTeamName.bind(this),
+      validate: this.validateRegisterInput.bind(this),
     })
 
     const { token } = await ux.prompt<{ token: string }>({
       type: 'input',
       name: 'token',
-      message: `\nProvide an access token for your secrets provider ${reset.green(
+      message: `\n🔐 Register secret storage access token ${reset.green(
         '→',
-      )}  \n🏀 ${white('TOKEN')} `,
+      )}  \n${white('Enter access token:')} `,
       afterMessage: `${reset.green('✓')} TOKEN    `,
-      // TODO: STEVE YOU NEED TO VALIDATE STUFF
-      // validate: this.validateTeamName.bind(this),
+      validate: this.validateRegisterInput.bind(this),
     })
 
     return {
+      activeTeam: inputs.activeTeam,
       url: url,
       token: token,
     } as RegisterInputs
@@ -83,125 +75,55 @@ export default class SecretsRegister extends Command {
   registerSecretsProvider = async (
     inputs: RegisterInputs,
   ): Promise<RegisterInputs> => {
+    try {
+      const { data: response } = await this.services.api.create(`teams/${inputs.activeTeam.name}/secrets/register`,
+        {
+          write_privilege: true,
+          token:          inputs.token,
+          url:            inputs.url,
+        },
+        {
+          headers: {
+            Authorization: this.accessToken,
+          },
+      });
 
-    console.log('OVER HERE MAKING BAD ASS REQUESTS')
-
-    return {} as RegisterInputs
+      return inputs
+    } catch (err) {
+      this.debug('%O', err)
+      throw new APIError(err)
+    }
   }
 
-  // getTeamsFromApi = async (inputs: SwitchInputs): Promise<SwitchInputs> => {
-  //   try {
-  //     const { data: teams } = await this.services.api.find('teams', {
-  //       headers: { Authorization: this.accessToken },
-  //     })
-  //     return { ...inputs, teams }
-  //   } catch (err) {
-  //     this.debug('%O', err)
-  //     throw new APIError(err)
-  //   }
-  // }
-
-  // setTeamsDisplayName = (inputs: SwitchInputs): SwitchInputs => {
-  //   const { teams, activeTeam } = inputs
-  //   const displayTeams = teams.map(t => {
-  //     // If the team is the user's active team, add custom styling to it
-  //     if (activeTeam && t.name === activeTeam.name) {
-  //       return {
-  //         ...t,
-  //         displayName: `${blue(t.name)} ${dim('[Active]')}`,
-  //       }
-  //     }
-  //     // If the team isn't the user's active team, simply copy the display name from the team name
-  //     return { ...t, displayName: t.name }
-  //   })
-  //   return { ...inputs, displayTeams }
-  // }
-
-  // getSelectedTeamPrompt = async (
-  //   inputs: SwitchInputs,
-  // ): Promise<SwitchInputs> => {
-  //   this.log("Here's the list of your teams:\n")
-  //   const { displayTeams } = inputs
-  //   const { teamSelected } = await ux.prompt<{ teamSelected: Team }>({
-  //     type: 'list',
-  //     name: 'teamSelected',
-  //     message: 'Select a team',
-  //     choices: displayTeams.map(team => {
-  //       return { name: team.displayName, value: team }
-  //     }),
-  //     bottomContent: `\n \n${white(
-  //       `Or, run ${italic.dim('ops help')} for usage information.`,
-  //     )}`,
-  //   })
-  //   this.log(`\n⏱  Switching teams`)
-  //   return { ...inputs, teamSelected }
-  // }
-
-  // updateActiveTeam = async (inputs: SwitchInputs): Promise<SwitchInputs> => {
-  //   try {
-  //     const {
-  //       teamSelected: { name, id },
-  //     } = inputs
-  //     const configData = await this.readConfig()
-  //     await this.writeConfig(configData, {
-  //       team: { name, id },
-  //     })
-  //   } catch (err) {
-  //     this.debug('%O', err)
-  //     throw new ConfigError(err)
-  //   }
-  //
-  //   return inputs
-  // }
-  //
-  // logMessage = (inputs: SwitchInputs): SwitchInputs => {
-  //   const {
-  //     teamSelected: { name },
-  //   } = inputs
-  //   this.log(`\n🚀 Huzzah! ${callOutCyan(name)} is now the active team.\n`)
-  //   return inputs
-  // }
-  //
-  // sendAnalytics = (config: Config) => (inputs: SwitchInputs) => {
-  //   const {
-  //     user: { email, username },
-  //   } = config
-  //   const {
-  //     activeTeam: { id: oldTeamId },
-  //     teamSelected: { id: newTeamId },
-  //   } = inputs
-  //   this.services.analytics.track(
-  //     {
-  //       userId: email,
-  //       cliEvent: 'Ops CLI Team:Switch',
-  //       event: 'Ops CLI Team:Switch',
-  //       properties: {
-  //         email,
-  //         username,
-  //         oldTeamId,
-  //         newTeamId,
-  //       },
-  //     },
-  //     this.accessToken,
-  //   )
-  // }
+  sendAnalytics = (user: User) => async (inputs: RemoveInputs) => {
+    const { email, username } = user
+    this.services.analytics.track(
+      {
+        userId: email,
+        teamId: this.team.id,
+        cliEvent: 'Ops CLI Register Secrets Provider',
+        event: 'Ops CLI Register Secrets Provider',
+        properties: {
+          email,
+          username,
+        },
+      },
+      this.accessToken,
+    )
+    return inputs
+  }
 
   async run() {
-    console.log("888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888 handling secrets like a boss")
-
     this.parse(SecretsRegister)
 
     try {
       await this.isLoggedIn()
 
-      console.log("THESE ARE THE INPUTS: ", "some inputs")
-
       const switchPipeline = asyncPipe(
         this.getActiveTeam,
-        this.checkHealth,
         this.promptForSecretsProviderCredentials,
         this.registerSecretsProvider,
-        // this.sendAnalytics(this.state.config),
+        this.sendAnalytics(this.user)
       )
       await switchPipeline()
     } catch (err) {
