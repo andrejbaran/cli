e0bbe64f1484bfbd615db82bbbc5627027b5bc54
Danielle Brook-Roberge <danielle+remax@hackcapital.com>
2020-05-14 14:05:27 -0700

    Clean up get-docker

--- src/utils/get-docker.ts
+++ src/utils/get-docker.ts
@@ -4,6 +4,64 @@ import * as fs from 'fs-extra'
 
 import { DOCKER_SOCKET } from '../constants/env'
 
+const INSTALL_DOCKER_MSG = `
+
+${ux.colors.reset.cyan(
+  "We're almost there! You'll just need to install Docker for CTO.ai ops to run properly - go here to install it now.",
+)}
+
+${ux.colors.reset.green('→')} https://docs.docker.com/install/
+${ux.colors.reset.grey(
+  "(You'll create an account with Docker in order to start the download)",
+)}
+
+Once installed, make sure you start the Docker app, then come back to this terminal and type ${ux.colors.reset.cyan(
+  "'Y'",
+)}.
+We'll be waiting right here when you're ready 👍\n`
+
+const UHOH_INSTALL_DOCKER_MSG = `
+
+${ux.colors.reset.cyan(
+  "Uh-oh! You'll just need to install Docker for CTO.ai ops to run properly - go here to install it now.",
+)}
+
+${ux.colors.reset.green('→')} https://docs.docker.com/install/
+${ux.colors.reset.grey(
+  "(You'll create an account with Docker in order to start the download)",
+)}
+
+Once installed, make sure you start the Docker app, then come back to this terminal and type ${ux.colors.reset.cyan(
+  "'Y'",
+)}.
+We'll be waiting right here when you're ready 👍\n`
+
+// TODO: This message is Mac-specific
+const DOCKER_NOT_RUNNING_MSG = `
+${ux.colors.reset.cyan(
+  "It looks like you have Docker installed, but it's not currently running.",
+)}
+${ux.colors.reset.cyan(
+  'Please start the Docker app to continue (You can find it in the MacOS → Applications folder)',
+)}
+
+Once Docker is running, come back to this terminal and type ${ux.colors.reset.cyan(
+  "'Y'",
+)}
+We'll be waiting right here when you're ready 👍\n
+`
+
+const DOCKER_STILL_NOT_RUNNING_MSG = `
+${ux.colors.reset.cyan("Hmm. Docker still doesn't seem to be running.")}
+${ux.colors.reset.cyan(
+  'Please check again, or run, "ops account support" and we\'ll be happy to help you out.',
+)}`
+
+const PLEASE_CHECK_AGAIN_MSG = `
+${ux.colors.reset.cyan(
+  'Please check that Docker is running again and come back here.',
+)}`
+
 /**
  * Helper function to display appropriate error message to the user
  * @param self Is the instance of 'this'
@@ -11,162 +69,30 @@ import { DOCKER_SOCKET } from '../constants/env'
  * @param type The type of error message we want to display
  */
 function logError(self: any, numRepeats: number, type: string) {
+  if (numRepeats >= 3) {
+    self.log(DOCKER_STILL_NOT_RUNNING_MSG)
+    return
+  }
+  if (numRepeats) {
+    self.log(PLEASE_CHECK_AGAIN_MSG)
+    return
+  }
+
+  // If we haven't retried we'll need specific messaging
   switch (type) {
     case 'account-create-docker-not-installed': {
-      if (!numRepeats) {
-        self.log(
-          `\n${ux.colors.reset.cyan(
-            "We're almost there! You'll just need to install Docker for CTO.ai ops to run properly - go here to install it now.",
-          )}`,
-        )
-        self.log(
-          `\n${ux.colors.reset.green('→')} https://docs.docker.com/install/`,
-        )
-        self.log(
-          `${ux.colors.reset.grey(
-            "(You'll create an account with Docker in order to start the download)",
-          )}`,
-        )
-        self.log(
-          `\nOnce installed, make sure you start the Docker app, then come back to this terminal and type ${ux.colors.reset.cyan(
-            "'Y'",
-          )}.`,
-        )
-        self.log("We'll be waiting right here when you're ready 👍\n")
-      } else if (numRepeats >= 3) {
-        self.log(
-          `\n${ux.colors.reset.cyan(
-            "Hmm. Docker still doesn't seem to be running.",
-          )}`,
-        )
-        self.log(
-          `${ux.colors.reset.cyan(
-            'Please check again, or run, "ops account support" and we\'ll be happy to help you out.',
-          )}`,
-        )
-      } else {
-        self.log(
-          `\n${ux.colors.reset.cyan(
-            'Please check that Docker is running again and come back here.',
-          )}`,
-        )
-      }
+      self.log(INSTALL_DOCKER_MSG)
       break
     }
     case 'account-create-docker-stopped': {
-      if (!numRepeats) {
-        self.log(
-          `\n${ux.colors.reset.cyan(
-            "It looks like you have Docker installed, but it's not currently running.",
-          )}`,
-        )
-        self.log(
-          `${ux.colors.reset.cyan(
-            'Please start the Docker app to continue (You can find it in the MacOS → Applications folder)',
-          )}`,
-        )
-        self.log(
-          `\nOnce Docker is running, come back to this terminal and type ${ux.colors.reset.cyan(
-            "'Y'",
-          )}`,
-        )
-        self.log("We'll be waiting right here when you're ready 👍\n")
-      } else if (numRepeats >= 3) {
-        self.log(
-          `\n${ux.colors.reset.cyan(
-            "Hmm. Docker still doesn't seem to be running.",
-          )}`,
-        )
-        self.log(
-          `${ux.colors.reset.cyan(
-            'Please check again, or run, "ops account support" and we\'ll be happy to help you out.',
-          )}`,
-        )
-      } else {
-        self.log(
-          `\n${ux.colors.reset.cyan(
-            'Please check that Docker is running again and come back here.',
-          )}`,
-        )
-      }
+      self.log(DOCKER_NOT_RUNNING_MSG)
       break
     }
     default:
       if (type.endsWith('docker-not-installed')) {
-        if (!numRepeats) {
-          self.log(
-            `\n${ux.colors.reset.cyan(
-              "Uh-oh! You'll need to install and run Docker for CTO.ai ops to work properly - go here to install it now.",
-            )}`,
-          )
-          self.log(
-            `\n${ux.colors.reset.green('→')} https://docs.docker.com/install/`,
-          )
-          self.log(
-            `${ux.colors.reset.grey(
-              "(You'll create an account with Docker in order to start the download)",
-            )}`,
-          )
-          self.log(
-            `\nOnce installed, make sure you start the Docker app, then come back to this terminal and type ${ux.colors.reset.cyan(
-              "'Y'",
-            )}.`,
-          )
-          self.log("We'll be waiting right here when you're ready 👍\n")
-        } else if (numRepeats >= 3) {
-          self.log(
-            `\n${ux.colors.reset.cyan(
-              "Hmm. Docker still doesn't seem to be running.",
-            )}`,
-          )
-          self.log(
-            `${ux.colors.reset.cyan(
-              'Please check again, or run, "ops account support" and we\'ll be happy to help you out.',
-            )}`,
-          )
-        } else {
-          self.log(
-            `\n${ux.colors.reset.cyan(
-              'Please check that Docker is running again and come back here.',
-            )}`,
-          )
-        }
+        self.log(UHOH_INSTALL_DOCKER_MSG)
       } else {
-        if (!numRepeats) {
-          self.log(
-            `\n${ux.colors.reset.cyan(
-              "It looks like you have Docker installed, but it's not currently running.",
-            )}`,
-          )
-          self.log(
-            `${ux.colors.reset.cyan(
-              'Please start the Docker app to continue (You can find it in the MacOS → Applications folder)',
-            )}`,
-          )
-          self.log(
-            `\nOnce Docker is running, come back to this terminal and type ${ux.colors.reset.cyan(
-              "'Y'",
-            )}`,
-          )
-          self.log("We'll be waiting right here when you're ready 👍\n")
-        } else if (numRepeats >= 3) {
-          self.log(
-            `\n${ux.colors.reset.cyan(
-              "Hmm. Docker still doesn't seem to be running.",
-            )}`,
-          )
-          self.log(
-            `${ux.colors.reset.cyan(
-              'Please check again, or run, "ops account support" and we\'ll be happy to help you out.',
-            )}`,
-          )
-        } else {
-          self.log(
-            `\n${ux.colors.reset.cyan(
-              'Please check that Docker is running again and come back here.',
-            )}`,
-          )
-        }
+        self.log(DOCKER_NOT_RUNNING_MSG)
       }
   }
 }
@@ -191,54 +117,46 @@ export default async function getDocker(
   self: any,
   type: string,
 ): Promise<Docker> {
-  // Point to the docker socket
-  /**
-   * Checks whether docker is installed on the machine
-   * Keep repeating until the socket is available
-   */
-  let numRepeats = 0 // Flag to set whether the prompt has been repeated or not
-  let isDockerInstalled = false // Flag to indicate whether docker is installed or not
-  while (!isDockerInstalled) {
+  let numRepeats = 0
+
+  // Check whether docker is installed on the machine
+  // Keep repeating until the socket is available
+  while (true) {
     try {
-      const stats = fs.statSync(DOCKER_SOCKET) // Resets the status of the socket
       // Check the validity of the socket, which is only available if docker is installed
-      isDockerInstalled = stats.isSocket()
+      if (fs.statSync(DOCKER_SOCKET).isSocket()) {
+        break
+      }
     } catch {
-      logError(self, numRepeats, `${type}-docker-not-installed`) // Log the error message
+      logError(self, numRepeats, `${type}-docker-not-installed`)
 
-      // Ask the user for prompt
       let { answer } = await confirmReadyContinue()
+      if (!answer) {
+        process.exit(0) // Exit safely if there is no docker without an error
+      }
 
-      // Return if user doesn't want to repeat
-      if (!answer) process.exit(0) // Exit safely if there is no docker without an error
-
-      numRepeats++ // Adds the number of errors the user has repeated
+      numRepeats++
     }
   }
 
-  /**
-   * Checks whether docker is running
-   * At this point, assume docker has already been installed
-   */
-  numRepeats = 0 // Re-initialize the flag for repeat
-  let isDockerRunning = false // Flag to indicate whether docker is running or not
+  // Check whether docker is running
+  // At this point, we can assume docker has already been installed
+  numRepeats = 0
   const docker = new Docker({ socketPath: DOCKER_SOCKET })
-  while (!isDockerRunning) {
-    // Instantiate a new docker client
+  while (true) {
     try {
-      await docker.ping() // Try to ping the docker daemon
-      // If successful, set flag to true to exit the while loop
-      isDockerRunning = true
+      await docker.ping()
+      // We don't need to retry if the ping succeeds
+      break
     } catch {
       logError(self, numRepeats, `${type}-docker-stopped`)
 
-      // Ask the user for prompt
       let { answer } = await confirmReadyContinue()
+      if (!answer) {
+        process.exit(0) // Exit safely if there is no docker without an error
+      }
 
-      // Return if user doesn't want to repeat
-      if (!answer) process.exit(0) // Exit safely if there is no docker without an error
-
-      numRepeats++ // Adds the number of errors the user has repeated
+      numRepeats++
     }
   }
   return docker
